eb4d1ed68901178484e541455680921b2cfbebd6
vaishupadmor <vaishupadmor@gmail.com>
2024-08-18 18:23:37 +0530

    code-added

---
+++ src/config/data.js
@@ -0,0 +1,48 @@
+
+const MAIN_TITLE = "Flower shoppy.."
+
+
+
+const TAG_LINE ="best flowers at one place"
+
+
+
+const THEME = {
+    main:"#D83A56",
+    highlight:"#66DE93",
+    primary:"FF616D",
+    secondary:"#B99470",
+    
+}
+const PRODUCTS =[
+    {
+      imgUrl:"https://img.freepik.com/free-photo/top-view-roses-flowers_23-2148860041.jpg",
+             title:"Roses"  ,
+             description:"fresh and prety flowers",
+             
+             price:2
+    },
+    {
+      imgUrl:"https://img.freepik.com/free-photo/fresh-yellow-flowers-green-foliage-dew_23-2148069268.jpg",
+             title:"Lily"  ,
+             description:"symbol of friendship and trust",
+             
+             price:3
+    },
+    {
+      imgUrl:"https://img.freepik.com/free-photo/beautiful-tropical-flowers_23-2148249121.jpg",
+      title:"Plumeria"  ,
+       description:"beautiful and fresh flowers",
+       
+       price:3.5
+    },
+    {
+      imgUrl:"https://img.freepik.com/free-photo/pink-white-lotus-flower-is-water_1340-32309.jpg",
+             title:"Lotus"  ,
+             description:"Represents divine love ",
+             
+    }]
+    
+
+    
+    export {PRODUCTS,MAIN_TITLE , TAG_LINE ,THEME }
---
+++ src/config/data1.js
@@ -0,0 +1,43 @@
+const MAIN_NAMES ="staff info.."
+const TAG_LINES ="we work to make world a beautiful place"
+
+const THEME = {
+    main:"#C0C78C",
+    highlight:"#A6B37D",
+    primary:"C0C78C",
+    secondary:"#B99470",
+    
+}
+
+
+const PERSONS =[
+    {
+      imgUrl:"https://img.freepik.com/free-photo/smiling-cute-female-student-casual-shirt_176420-18233.jpg",
+           title:"prajakta"  ,
+           description:"BCS (bachlors of computer science) ",
+
+           
+    },
+    {
+        imgUrl:"https://img.freepik.com/free-photo/indoor-studio-shot-attractive-beautiful-pretty-young-woman-wearing-eyeglasses-yellow-sweatshirt-having-long-fair-hair-posing-isolated-pink-wall-people-emotions-concept_176532-6755.jpg",
+             title:"Nikita"  ,
+             description:"BE (bacholer of engineering)",
+             
+             
+      },
+      {
+        imgUrl:"https://img.freepik.com/free-photo/portrait-smiling-blonde-woman_23-2148316635.jpg",
+             title:"Prerna"  ,
+             description:"IT (information technology",
+             
+             
+      },
+      {
+        imgUrl:"https://img.freepik.com/free-photo/indoor-shot-serious-self-assured-teenage-girl-keeps-hands-crossed-wears-checkered-shirt_176532-9582.jpg",
+             title:"Vaishali"  ,
+             description:"Computer engineer",
+             
+             
+      }
+  ]
+  export {PERSONS , MAIN_NAMES , TAG_LINES,THEME}
